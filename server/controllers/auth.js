f37e1d2ac2b7ee3cd24933e099c314ce8450c28c
Kshitij Thatte <kshitijthatte@gmail.com>
2021-02-22 21:54:45 +0530

    added validation to signup route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,15 @@
 const User = require("../models/user");
+const { body, validationResult } = require("express-validator");
 
 exports.signup = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ errors: errors.array()[0].msg, param: errors.array()[0].param });
+  }
+
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 const { signup, signout } = require("../controllers/auth");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  body("name")
+    .isLength({ min: 3 })
+    .withMessage("name should be at least 3 chars"),
+  body("email").isEmail().withMessage("email is requires"),
+  body("password")
+    .isLength({ min: 3 })
+    .withMessage("password should be at least 3 chars"),
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
