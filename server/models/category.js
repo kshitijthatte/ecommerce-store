3f51667d5ba468b02d06cb9d1c95979e2486c13f
Kshitij Thatte <kshitijthatte@gmail.com>
2021-02-22 13:27:45 +0530

    added category and product schema

---
+++ server/models/category.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Category", categorySchema);
--- server/models/user.js
+++ server/models/user.js
@@ -4,42 +4,47 @@ const { v4: uuidv4 } = require("uuid");
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    maxlength: 32,
-    trim: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      maxlength: 32,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    encry_password: {
+      type: String,
+      required: true,
+    },
+    userinfo: {
+      type: String,
+      trim: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
   },
-  lastname: {
-    type: String,
-    maxlength: 32,
-    trim: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  encry_password: {
-    type: String,
-    required: true,
-  },
-  userinfo: {
-    type: String,
-    trim: true,
-  },
-  salt: String,
-  role: {
-    type: Number,
-    default: 0,
-  },
-  purchases: {
-    type: Array,
-    default: [],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema
   .virtual("password")
