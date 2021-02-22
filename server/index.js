ef015d50db96750cc5772ee52d7945548d4c43e7
Kshitij Thatte <kshitijthatte@gmail.com>
2021-02-22 18:20:53 +0530

    added database connectivity

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
