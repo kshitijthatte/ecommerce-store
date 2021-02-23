f355ccfb1c253329cc9050ea23ad2741a53a8fa0
Kshitij Thatte <kshitijthatte@gmail.com>
2021-02-23 21:41:14 +0530

    added authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
   const errors = validationResult(req);
@@ -21,8 +23,72 @@ exports.signup = (req, res) => {
   });
 };
 
-exports.signout = (req, res) => {
-  res.json({
-    message: "user signout",
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ errors: errors.array()[0].msg, param: errors.array()[0].param });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      res.status(400).json({
+        error:
+          "The email you entered doesn't belong to any account. Please check and try again.",
+      });
+    }
+
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error:
+          "The email and password you entered did not match. Please check and try again.",
+      });
+    }
+
+    // create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    // put token in cookie
+    res.cookie("token", token, { expire: new Date() + 9999 });
+
+    // send response to front end
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role } });
   });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie();
+  res.json({
+    message: "User signout successful",
+  });
+};
+
+// protected routes
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth",
+  algorithms: ["HS256"],
+});
+
+// custom middleware
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED",
+    });
+  }
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "You do not have Administrator privileges, ACCESS DENIED",
+    });
+  }
+  next();
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
-const { signup, signout } = require("../controllers/auth");
+const { signup, signin, signout, isSignedIn } = require("../controllers/auth");
 
 router.post(
   "/signup",
   body("name")
     .isLength({ min: 3 })
-    .withMessage("name should be at least 3 chars"),
-  body("email").isEmail().withMessage("email is requires"),
+    .withMessage("Name should be at least 3 chars"),
+  body("email").isEmail().withMessage("Email is required"),
   body("password")
     .isLength({ min: 3 })
-    .withMessage("password should be at least 3 chars"),
+    .withMessage("Password should be at least 3 chars"),
   signup
 );
+
+router.post(
+  "/signin",
+  body("email").isEmail().withMessage("Email is required"),
+  body("password").isLength({ min: 1 }).withMessage("Password is required"),
+  signin
+);
+
 router.get("/signout", signout);
 
 module.exports = router;
