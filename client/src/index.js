9b6eeba687cc75f0d83099dee3d3d251aa320d6c
Kshitij Thatte <kshitijthatte@gmail.com>
2021-02-26 09:24:28 +0530

    installed react and tailwind CSS for frontend

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
+import "./index.css";
+
+ReactDOM.render(<Routes></Routes>, document.getElementById("root"));
